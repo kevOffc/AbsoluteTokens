f352516c9aa8ebd11498dbe8acd4894fc33921db
kevOffc <mtwska0@gmail.com>
2025-02-16 18:34:02 +0700

    Update Tokens.js

--- Tokens.js
+++ Tokens.js
@@ -1,5 +1,5 @@
-const TokenUsers = [
-  '7698882880:AAHqOZIyuK4ioALuHFFBJp5CkGM4uvCL4ZU' 
-];
-
-module.exports = TokenUsers;
+{
+    "tokens": [
+    "7698882880:AAHqOZIyuK4ioALuHFFBJp5CkGM4uvCL4ZU"
+    ]
+}
